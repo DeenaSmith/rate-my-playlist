ae7d4b67a546606baf42aef0afa222e85b478ac4
teachergg <jwhitney@bisd.us>
2022-05-15 10:43:10 -0500

    added form handling for playlist input

---
+++ public/javascript/add-playlist.js
@@ -0,0 +1,25 @@
+async function newFormHandler(event) {
+  event.preventDefault();
+
+  const playlistUrlInput = document.querySelector('input[name="playlist-url"]').value;
+  const playlistUrlSplit = playlistUrlInput.split('/');
+  const playlist_url = playlistUrlSplit[4];
+
+  const response = await fetch(`/api/playlists`, {
+    method: 'POST',
+    body: JSON.stringify({
+      playlist_url
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document.querySelector('.new-playlist-form').addEventListener('submit', newFormHandler);
